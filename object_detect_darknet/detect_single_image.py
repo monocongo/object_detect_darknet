2a0d880eba554654f16ffb77dbe9feb94143edfa
monocongo <monocongo@gmail.com>
2019-10-23 14:51:38 -0400

    added resizing for display to account for large images
    #2

--- object_detect_darknet/detect_single_image.py
+++ object_detect_darknet/detect_single_image.py
@@ -4,10 +4,10 @@ displayed including detection bounding boxes.
 
 Example usage:
 
-$ python3 detect_single_image.py --images_dir \
-    --weights /home/james/darknet/yolov3/20190927/yolov3.weights \
-    --config /home/james/darknet/yolov3/20190927/yolov3.cfg \
-    --labels /home/james/darknet/yolov3/20190927/labels.txt \
+$ python3 detect_single_image.py --images_dir /data/datasets/weapons/test \
+    --weights /home/james/darknet/20191004/yolov3-tiny-weapons-416_final.weights \
+    --config /home/james/darknet/20191004/yolov3-tiny-weapons-416.cfg \
+    --labels /home/james/darknet/20191004/labels.txt \
     --confidence 0.6
 """
 
@@ -121,6 +121,62 @@ def detect_objects(
 
 
 # ------------------------------------------------------------------------------
+def resize_image(
+        image: np.ndarray,
+        new_width: int,
+        new_height: int,
+) -> (np.ndarray, float, float):
+    """
+    Reads image data from a file and resizes it to the specified dimensions,
+    preserving the aspect ratio and padding on the right and bottom as necessary.
+
+    :param image_file_path:
+    :param new_width:
+    :param new_height:
+    :return:
+    """
+
+    # get the dimensions and aspect ratio
+    original_height, original_width = image.shape[:2]
+    aspect_ratio = original_width / original_height
+
+    # determine the interpolation method we'll use
+    if (original_height > new_height) or (original_width > new_width):
+        # use a shrinking algorithm for interpolation
+        interp = cv2.INTER_AREA
+    else:
+        # use a stretching algorithm for interpolation
+        interp = cv2.INTER_CUBIC
+
+    # determine the new width and height (may differ from the width and
+    # height arguments if using those doesn't preserve the aspect ratio)
+    final_width = new_width
+    final_height = round(final_width / aspect_ratio)
+    if final_height > new_height:
+        final_height = new_height
+    final_width = round(final_height * aspect_ratio)
+
+    # at this point we may be off by a few pixels, i.e. over
+    # the specified new width or height values, so we'll clip
+    # in order not to exceed the specified new dimensions
+    final_width = min(final_width, new_width)
+    final_height = min(final_height, new_height)
+
+    # get the padding necessary to preserve aspect ratio
+    pad_bottom = abs(new_height - final_height)
+    pad_right = abs(new_width - final_width)
+
+    # scale and pad the image
+    scaled_img = cv2.resize(image, (final_width, final_height), interpolation=interp)
+    padded_img = cv2.copyMakeBorder(
+        scaled_img, 0, pad_bottom, 0, pad_right,
+        borderType=cv2.BORDER_CONSTANT, value=[0, 0, 0],
+    )
+
+    return padded_img
+
+
+# ------------------------------------------------------------------------------
 if __name__ == '__main__':
 
     # parse the command line arguments
@@ -177,7 +233,13 @@ if __name__ == '__main__':
         image_file_path = os.path.join(args["images_dir"], image_file_name)
 
         # perform object detection on the image, get the image annotated with bounding boxes
-        image = detect_objects(image_file_path, darknet, args["confidence"])
+        image = detect_objects(image_file_path, darknet, labels, label_colors, args["confidence"])
+
+        # resize to fit on a screen (for handling especially large images)
+        display_width = 800
+        display_height = 400
+        if (image.shape[0] > display_height) or (image.shape[1] > display_width):
+            image = resize_image(image, display_width, display_height)
 
         # show the output image
         cv2.imshow("Image", image)
