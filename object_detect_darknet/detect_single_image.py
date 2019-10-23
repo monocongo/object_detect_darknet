939ece368899c5d686b905e41dbc09996bacd371
monocongo <monocongo@gmail.com>
2019-10-23 13:20:19 -0400

    now getting output layer names, labels and associated colors, building a BLOB, and passing it through the model
    #2

--- object_detect_darknet/detect_single_image.py
+++ object_detect_darknet/detect_single_image.py
@@ -2,6 +2,7 @@ import argparse
 import os
 
 import cv2
+import numpy as np
 
 # non-maximum suppression threshold
 _NMS_THRESHOLD = 0.3
@@ -46,6 +47,15 @@ if __name__ == '__main__':
     # load the model from weights/configuration
     darknet = cv2.dnn.readNetFromDarknet(args["config"], args["weights"])
 
+    # get the output layer names
+    layer_names = darknet.getLayerNames()
+    layer_names = [layer_names[i[0] - 1] for i in darknet.getUnconnectedOutLayers()]
+
+    # read labels into a list, get colors to represent each
+    labels = open(args["labels"]).read().strip().split("\n")
+    np.random.seed(42)
+    label_colors = np.random.randint(0, 255, size=(len(labels), 3), dtype="uint8")
+
     # loop over each image file in the specified images directory
     image_file_names = os.listdir(args["images_dir"])
     for image_file_name in image_file_names:
@@ -53,9 +63,20 @@ if __name__ == '__main__':
         # find any images with unexpected dimensions
         image_file_path = os.path.join(args["images_dir"], image_file_name)
 
-        # TODO read image into BLOB
+        # read image into BLOB (assuming model resolution is 416x416)
+        image = cv2.imread(args["image"])
+        (height, width) = image.shape[:2]
+        blob = cv2.dnn.blobFromImage(
+            image,
+            1 / 255.0,
+            (416, 416),
+            swapRB=True,
+            crop=False,
+        )
 
-        # TODO pass BLOB through model to get detections
+        # pass the BLOB through the model to get detections
+        darknet.setInput(blob)
+        layer_outputs = darknet.forward(layer_names)
 
         # TODO draw detections onto image, display image
 
